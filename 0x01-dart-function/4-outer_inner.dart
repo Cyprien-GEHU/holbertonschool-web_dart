77aa4601fef7d0e3197739c79c2bf930625108cd
Cyprien GEHU <cypriengehu@gmail.com>
2026-04-13 11:58:01 +0200

    feat: Function task 4 -  Outer Inner

---
+++ 0x01-dart-function/4-outer_inner.dart
@@ -0,0 +1,8 @@
+void outer(String name, String id) {
+  String inner() {
+    List<String> fullName = name.split(" ");
+
+    return "Hello Agent ${fullName[1][0]}.${fullName[0]} your id is $id";
+  }
+  print(inner());
+}
