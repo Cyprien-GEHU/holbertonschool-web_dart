cefeea12fe68987251fae8e62dada7e77d7fe603
Cyprien GEHU <cypriengehu@gmail.com>
2026-04-13 10:55:44 +0200

    fix: Hello Dart task 5 -  remplace if and else by assert

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,8 +1,6 @@
 void main(List<String> args) {
   int score = int.parse(args[0]);
-  if (score >= 80) {
-    print("You Passed");
-  } else {
-    print("The score must be bigger or equal to 80");
-  }
+  assert(score >= 80, "The score must be bigger or equal to 80");
+  print("You Passed");
+  
 }
