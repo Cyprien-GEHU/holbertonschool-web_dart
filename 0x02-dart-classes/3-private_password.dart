364b3ab757ed534c5e9c955f1013b3e8f0c0cfc2
Cyprien GEHU <cypriengehu@gmail.com>
2026-04-14 12:42:08 +0200

    feat: Dart Classe task 3 - Private Password

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,29 @@
+class Password{
+  String _password = "";
+
+  Password({required String password}) : this._password = password;
+
+  set password(String value){
+    _password = value;
+  }
+
+  String get password => _password;
+
+  bool isValid() {
+    if (8 > password.length || password.length > 16)
+    {
+      return false;
+    }
+
+    bool upper = password.contains(RegExp(r'[A-Z]'));
+    bool lower = password.contains(RegExp(r'[a-z]'));
+    bool number = password.contains(RegExp(r'[0-9]'));
+
+    return upper && lower && number;
+  }
+
+  @override
+  String toString(){
+    return "Your Password is: $password";
+  }
+}
